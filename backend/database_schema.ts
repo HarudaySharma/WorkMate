c50f8202002cc7295fb4e3a8772ef7df50baf5ab
HarudaySharma <harudayvasist@gmail.com>
2025-02-27 15:16:27 +0530

    brainstroming database schema

---
+++ backend/database_schema.ts
@@ -0,0 +1,71 @@
+type User = {
+    id: string, // (Primary Key)
+	name: string,
+	password: string // (hashedPass)
+	userName: string // (unique)
+    email: string, // (unique)
+    isEmailVerified: boolean,// make email verified.
+	profilePicture: string
+    createdAt: Date,
+    updatedAt: Date,
+    // optional:
+    // status: "online" | "offline"
+}
+
+
+type WorkSpace = {
+	// each workspace is a container for all kinds of functionality
+	id: string, // workSpace id
+	inviteLink: string, // to let users join this particular workspace
+	name: string,
+	members: Member[], // join requests should also be there.
+    chats: string[] // Foreign Key (Chat.id)
+    // multiple chats in one workspace (one-one or group chats)
+}
+
+type Chat = {
+    id: string, // chat id
+	type: 'group' | 'one-one',
+    workspaceID: string // Foreign Key (WorkSpace.id)
+    lastMessageAt: Date,
+}
+
+type Message = {
+	sender: string, // Foreign Key (User.id)
+	reciever: string, // Foreign Key (User.id)
+    chatID: string // Foreign Key (Chat.id)
+
+	type: "text" | "image" | "audio",
+	text: string,
+	image: string, // url to the image
+	audio: string, // url to the audio file
+    createdAt: Date,
+    // optional:
+    // isDeleted: boolean
+    // isUpdated: boolean
+}
+
+type Member = {
+	userId: string, // Foreign Key (User.id)
+	role: "Admin" |  "User" |  "...",
+    kandbans: Kanban,
+}
+
+type Kanban = { // Board
+    lists: List[],
+    visibility: "members" | "personal" | "anyone",
+}
+
+type List = {
+    name: string,
+    cards: Card[],
+}
+
+type Card = {
+    name: string,
+    description: string,
+    deuDate: Date,
+}
+
+//your cards, lists, due dates, and more to keep you organized and on track.
+
